d8670a2641e83db1de86d36d78c2b1f1f59b0250
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-09 13:40:15 +0800

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();     // express routers
 const multer = require('multer');   // upload photo
 
-let Products = require("../models/products.model");
+let Products = require("../models/products");
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -27,11 +27,7 @@ router.post("/add", upload.single("productPhoto"), (req, res) => {   // post req
   const newProduct = new Products({
     productName: req.body.productName,
     price: Number(req.body.price),
-    condition: req.body.condition,
     productDescription: req.body.productDescription,
-    ownerId: req.body.ownerId,
-    ownerDisName: req.body.ownerDisName,
-    ownerEmail: req.body.ownerEmail,
     productPhoto: req.file.originalname,
   });
 
@@ -39,31 +35,6 @@ router.post("/add", upload.single("productPhoto"), (req, res) => {   // post req
   newProduct.save()        // save the new user to DB
      .then(() => res.json("Product uploaded!"))        // return "User added" if add success
      .catch(err => res.status(400).json("Error: " + err));   // return error if failed
-});
-
-router.get("/:id",(req, res) => {        
-  // id object was created by mongo automatically since object created , get request, return that product photo by that id
-  Products.findById(req.params.id)          // findByID
-    .then((product) => res.json(product))       // return as json
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.put("/update/:id", upload.single("productName"),(req, res) => {    //update data of the object by id
-  Products.findById(req.params.id)
-  .then((product) => {
-      /* start of the update from the post request, received from route('/update/:id') */
-      product.productName = req.body.productName;
-      product.price = Number(req.body.price);
-      product.condition = req.body.condition;
-      product.productDescription = req.body.productDescription;
-      // product.productPhoto = req.file.originalname;
-      /* end */
-
-    product.save()
-      .then(() => res.json("Product Updated!"))
-      .catch(err => res.status(400).json("Error: " + err));
-  })
-  .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.delete("/:id",(req, res) => {     //pass in object id, delete request, delete that object by id
