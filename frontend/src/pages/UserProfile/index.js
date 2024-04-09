6250306f982e03f18ad521cf94856ac7896f3c4d
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-09 22:49:40 +0800

    Update index.js

--- frontend/src/pages/UserProfile/index.js
+++ frontend/src/pages/UserProfile/index.js
@@ -1,23 +1,46 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import './style.css';
 
+const User = () => {
+    const [userEmail, setUserEmail] = useState('');
 
-//function needs to be Capital Letter in the first
-const Profile = ({ userId, maskBackgroundRef }) => {
-    const { showNotification } = useNotification();
-  useEffect(() => {
-    if (maskBackgroundRef.current) {
-      maskBackgroundRef.current.scrollTo(0, 0);
-    }
-  }, [maskBackgroundRef]);
+    useEffect(() => {
+        // Since the token is stored in cookies, we include credentials in our fetch request.
+        // The browser will automatically handle sending the appropriate cookies.
+        fetch('http://localhost:5001/api/auth/authenticate', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include', // Ensure cookies, including auth tokens, are included in the request
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Token validation failed');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setUserEmail(data.email); // Set user email if token is valid
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, []); // The effect runs once after the component mounts
 
-  return (
-    <>
-      <div className="col-lg-6" id="content">
-          <ProfileContent/>
-      </div>
-    </>
-  );
+    return (
+        <div className="user">
+            <Header />
+            <main>
+                <h1>User Profile</h1>
+                {userEmail && <p>User Email: {userEmail}</p>}
+                
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
-export default Profile;
+export default UserProfile;
