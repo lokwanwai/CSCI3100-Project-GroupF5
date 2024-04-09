ec6e62cc52f5d1bc384fe11bc5183109e8d3c479
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-09 23:04:28 +0800

    Update index.js

--- frontend/src/pages/UserProfile/index.js
+++ frontend/src/pages/UserProfile/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import './style.css';
@@ -36,7 +37,8 @@ const User = () => {
             <main>
                 <h1>User Profile</h1>
                 {userEmail && <p>User Email: {userEmail}</p>}
-                
+                <li><Link to="/cart">My Cart</Link></li>
+                <li><Link to="/order">My order</Link></li>
             </main>
             <Footer />
         </div>
