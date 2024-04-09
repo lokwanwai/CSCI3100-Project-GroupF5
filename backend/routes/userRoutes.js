3bc97e2c121a4afc348cec7289c998f5a2f44a05
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-09 21:09:22 +0800

    Update userRoutes.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const Account = require("../model/user");
+const router = express.Router();
+
+// Update user profile
+router.patch("/profile/:userId", async (req, res) => {
+  try {
+    await Account.updateOne(
+      { userId: req.params.userId },
+      {
+        $set: {
+          userEmail: req.body.userEmail,
+          userName: req.body.userName,
+        },
+      }
+    );
+
+    // Send the response here
+    res.status(200).json({
+      message: "Profile updated",
+    });
+  } catch (err) {
+    res.status(401).json({ message: err });
+  }
+});
+
+// Update password of a user
+router.patch("/password/reset", async (req, res) => {
+  try {
+    const user = await Account.findOne({
+      userId: req.body.userId,
+    });
+    if (!user) {
+      return res.status(404).send("User Not Found.");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    await Account.updateOne(
+      {
+        userId: req.body.userId,
+      },
+      { $set: { password: hashedPassword } }
+    );
+    res.status(200).send("Password has been successfully reset.");
+  } catch (err) {
+    res
+      .status(500)
+      .send("Internal server error. Please try again or contact admin.");
+  }
+});
+
+// Get all the users info
+router.get("/", async (req, res) => {
+  try {
+    const list = await Account.find();
+    res.json(list);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
