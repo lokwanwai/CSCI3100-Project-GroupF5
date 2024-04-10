fb30b01c20abc4d63590b8a9e102ac2609603b43
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-10 20:53:13 +0800

    Update index.js

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
                     <li><Link to="/shipping">Shipping and return policy</Link></li>
                     <li><Link to="/faq">FAQ</Link></li>
                     <li>Tel: +852 12345678</li>
-                    <li>E-mail: csci3100gp5@gmail.com</li>
+                    <li>E-mail: csci3100gpf5@gmail.com</li>
                 </ul>
             </nav>
         </footer>
