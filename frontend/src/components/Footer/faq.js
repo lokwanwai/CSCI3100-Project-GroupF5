e9246055f5570c6cb8e249eeef6f2f7dc30ccde8
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-10 20:52:12 +0800

    Create faq.js

---
+++ frontend/src/components/Footer/faq.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+
+const faq = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+              <p> This is FAQ </p>
+            </main>
+            <Footer />
+        <div/>
+    );
+};
+
+export default faq;
