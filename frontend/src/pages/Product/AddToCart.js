2846de65b2282154cda4ca4340a7454e021870f8
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-11 01:54:24 +0800

    Update AddToCart.js

--- frontend/src/pages/Product/AddToCart.js
+++ frontend/src/pages/Product/AddToCart.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const Cart = ({ product, onAddcart }) => 
+const AddToCart = ({ product, onAddcart }) => {
 	const [itemCount, setItemCount] = React.useState(1);
 	const isDisabled = itemCount < 1;
 	return (
@@ -13,34 +11,34 @@ const Cart = ({ product, onAddcart }) =>
 				<i class="bi bi-dash-circle" onClick={() => {
     					setItemCount(Math.max(itemCount - 1, 0));
     				}}>
-    					{" "}
+    					<div style={{ fontSize: '20px', color: '#111111' }}>
+                        	${itemCount}
+                    	</div>
     				</i>
     				<i class="bi bi-plus-circle" onClick={() => {
     						setItemCount(itemCount + 1);
-    				}}>
+    				}}></i>
 				<button
-		                    	className="btn add-button"
-		                    	disabled={isDisabled}
-					onClick={() => {onAddcart(product, itemCount)
-		                   	style={{
-			                        backgroundColor: '#111111',
-			                        color: '#FFFFFF',
-			                        borderRadius: '40px',
-			                        padding: '12px 24px',
-			                        fontSize: '18px',
-			                        fontWeight: 'bold',
-			                        border: 'none',
-			                        cursor: isDisabled ? 'not-allowed' : 'pointer',
-			                        opacity: isDisabled ? 0.6 : 1,
-			                    }}>
-    					}}
-    				>
-    					{"Add to Cart"}
+		            className="btn add-button"
+		            disabled={isDisabled}
+					onClick={() => {onAddcart(product, itemCount)}}
+		            style={{
+			            backgroundColor: '#111111',
+			            color: '#FFFFFF',
+			            borderRadius: '40px',
+			            padding: '12px 24px',
+			            fontSize: '18px',
+			            fontWeight: 'bold',
+			            border: 'none',
+			            cursor: isDisabled ? 'not-allowed' : 'pointer',
+			            opacity: isDisabled ? 0.6 : 1,
+			        }}>
+    				"Add to Cart"
 				</button>
     			</div>
     		</div>
     	);
-    }
-};
+    };
 
 export default AddToCart;
+
