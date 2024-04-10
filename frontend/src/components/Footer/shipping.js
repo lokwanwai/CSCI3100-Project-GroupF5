7d07b320738e8c0a351f1512ce0de7546017a501
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-10 20:48:41 +0800

    Create shipping.js

---
+++ frontend/src/components/Footer/shipping.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const shipping = () => {
+    return (
+        <main>
+          <p> This is shipping policy </p>
+        </main>
+    );
+};
+
+export default shipping;
