60730f53f403050014d9260f68651b016cd78f04
choihm1234 <153415546+choihm1234@users.noreply.github.com>
2024-04-10 21:26:09 +0800

    Update ChangeEmail.js

--- frontend/src/pages/UserProfile/ChangeEmail.js
+++ frontend/src/pages/UserProfile/ChangeEmail.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+const ChangeEmail  = ({ user, changeEmail }) => {
+    return (
+        <tr>
+            <button
+                className="btn btn-outline-secondary btn-sm"
+                type="button"
+                onClick={() => changeEmail(user.userName)}
+            >
+                Change e-mail address
+            </button>
+        </tr>
+    );
+};
+
+export default ChangeEmail;
