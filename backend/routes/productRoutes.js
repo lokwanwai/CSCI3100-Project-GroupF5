642dfab90c165cf733633929d47a09e2de3ce410
Ken Yeung <kyeunga25@gmail.com>
2024-04-10 15:14:55 +0800

    Add sample product router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,16 +2,37 @@
 const express = require('express');
 const router = express.Router();
 
-// Define your routes and handlers
-router.get('/', (req, res) => {
-    // Handle GET request for products
-    res.send('GET request for products');
+let Products = require("../models/product");
+
+router.delete("/delete-product/:id", async (req, res) => {
+    try {
+        const { productId } = req.params;
+
+        // Find the cart item based on userEmail and productId
+        const targetProduct = await Products.findOne({ productId });
+
+        if (!targetProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        // Remove the cart item
+        await Products.deleteOne({ productId });
+
+        res.json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error('Error removing product:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-router.post('/', (req, res) => {
-    // Handle POST request for creating a new product
-    res.send('POST request for products');
+router.get('/', async (req, res) => {
+    try {
+        const list = await Products.find();
+        res.json(list);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-// Export the router instance
 module.exports = router;
